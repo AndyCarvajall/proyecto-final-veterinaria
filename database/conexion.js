7e360157f5ebf96fb06bf7c85bb99b3c852fdee3
Andy <andersonst07@gmail.com>
2023-07-10 17:19:15 -0500

    Cambios realizados de Veterinaria a Hospital

--- database/conexion.js
+++ database/conexion.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
-  database: 'veterinaria'
+  password: 'Anderson2202',
+  database: 'hospital'
 });
 
 // Conexión a la base de datos
